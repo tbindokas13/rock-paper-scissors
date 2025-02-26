041f543a9ee60b7e7955ca1ce7160c1135361074
Tomas Bindokas <31992244+tbindokas13@users.noreply.github.com>
2025-02-26 11:15:14 -0600

    Add: Human choice input prompt & global scope variables for scope keeping

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-console.log("Hello World!");
+console.log("Welcome to Rock-Paper-Scissors console edition!");
 
-function getComputerChoice(){
-    let number=Math.floor(Math.random()*3+1);
-    return number===1? "rock" : 
-            number===2 ? "paper": "scissors"; 
+//START OF GLOBAL SCOPE
+let humanScore = 0;
+let computerScore = 0;
+//END OF GLOBAL SCOPE
+
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * 3 + 1);
+  return number === 1 ? "rock" : number === 2 ? "paper" : "scissors";
 }
 
+//NEED TO: Implement capitalization cases (ex. Rock vs rock)
+function getHumanChoice() {
+  let choice = prompt("Please pick: Rock, Paper or Scissors");
+  if (choice === "Rock" || choice === "Paper" || choice === "Scissors")
+    console.log("You picked: " + choice);
+  else console.log("Invalid choice!: " + choice);
+}
