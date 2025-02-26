43f23da878e6d085d574174ae038909bfece9eb3
Tomas Bindokas <31992244+tbindokas13@users.noreply.github.com>
2025-02-26 15:28:09 -0600

    Add: Playing a game round

--- main.js
+++ main.js
@@ -10,10 +10,61 @@ function getComputerChoice() {
   return number === 1 ? "rock" : number === 2 ? "paper" : "scissors";
 }
 
-//NEED TO: Implement capitalization cases (ex. Rock vs rock)
 function getHumanChoice() {
   let choice = prompt("Please pick: Rock, Paper or Scissors");
-  if (choice === "Rock" || choice === "Paper" || choice === "Scissors")
-    console.log("You picked: " + choice);
-  else console.log("Invalid choice!: " + choice);
+  return choice;
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if (
+    !(
+      humanChoice.toLowerCase() === "rock" ||
+      humanChoice.toLowerCase() === "paper" ||
+      humanChoice.toLowerCase() === "scissors"
+    )
+  )
+    return console.log("Invalid choice!: " + humanChoice);
+  else {
+    console.log("Computer chose: " + computerChoice);
+    switch (humanChoice) {
+      case "rock":
+        if (computerChoice === "rock") console.log("It's a tie!");
+        else if (computerChoice === "paper") {
+          console.log("You lost! :(");
+          computerScore++;
+        } else {
+          console.log("You won! :)");
+          humanScore++;
+        }
+        break;
+      case "paper":
+        if (computerChoice === "rock") {
+          console.log("You won! :)");
+          humanScore++;
+        } else if (computerChoice === "paper") {
+          console.log("It's a tie!");
+        } else {
+          console.log("You lost! :(");
+          computerScore++;
+        }
+        break;
+      case "scissors":
+        if (computerChoice === "rock") {
+          console.log("You lost! :(");
+          computerScore++;
+        } else if (computerChoice === "paper") {
+          console.log("You won! :)");
+          humanScore++;
+        } else {
+          console.log("It's a tie!");
+        }
+    }
+  }
+  console.log("Your Score: " + humanScore);
+  console.log("Computer Score: " + computerScore);
+}
+
+playRound(humanSelection, computerSelection);
